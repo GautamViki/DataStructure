0f77c08d2af1a837106df1930738d77f23f018f0
GautamViki <vikacse@gmail.com>
2022-10-13 11:45:51 +0530

    preorder traverse

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -1,12 +1,16 @@
 package BinaryTree;
 
+import java.util.Stack;
+
 public class BinaryTree {
     private TreeNode root;
 
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
         binaryTree.createTree();
-        binaryTree.preOrderTraverse(binaryTree.root);
+        System.out.println("Recursive way");
+        binaryTree.preOrderTraverseRecursively(binaryTree.root);
+        binaryTree.preOrderTraverseIteratively(binaryTree.root);
     }
 
     public void createTree() {
@@ -25,11 +29,28 @@ public class BinaryTree {
         node2.right = node5;
     }
 
-    public void preOrderTraverse(TreeNode root) {
+    public void preOrderTraverseRecursively(TreeNode root) {
         if (root == null) return;
         System.out.print(root.data + " ");
-        preOrderTraverse(root.left);
-        preOrderTraverse(root.right);
+        preOrderTraverseRecursively(root.left);
+        preOrderTraverseRecursively(root.right);
+    }
+
+    public void preOrderTraverseIteratively(TreeNode root) {
+        System.out.println("\nIterative way");
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            System.out.print(temp.data + " ");
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
     }
 
     private class TreeNode {
