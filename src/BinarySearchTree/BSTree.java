eb54e4c2ab2bbf54e443740de59aeddca3ae9938
GautamViki <vikacse@gmail.com>
2022-10-15 01:51:13 +0530

    Binary Search Tree

---
+++ src/BinarySearchTree/BSTree.java
@@ -0,0 +1,19 @@
+package BinarySearchTree;
+
+public class BSTree {
+    private TreeNode root;
+
+    public static void main(String[] args) {
+
+    }
+
+    private class TreeNode {
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+}
