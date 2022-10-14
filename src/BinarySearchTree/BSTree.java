302dc0ce1dd23ec40a2e4e23f139016c9676eae6
GautamViki <vikacse@gmail.com>
2022-10-15 02:08:31 +0530

    Binary Search Tree inser method

--- src/BinarySearchTree/BSTree.java
+++ src/BinarySearchTree/BSTree.java
@@ -7,6 +7,23 @@ public class BSTree {
 
     }
 
+    public void insertNode(int data) {
+        root = insertNode(root, data);
+    }
+
+    public TreeNode insertNode(TreeNode root, int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return root;
+        }
+        if (data < root.data) {
+            root.left = insertNode(root.left, data);
+        } else {
+            root.right = insertNode(root.right, data);
+        }
+        return root;
+    }
+
     private class TreeNode {
         private int data;
         private TreeNode left;
