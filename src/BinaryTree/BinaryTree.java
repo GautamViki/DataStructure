25974c87d2f342626d6a7f422a3276dd3f134bfd
GautamViki <vikacse@gmail.com>
2022-10-15 01:19:53 +0530

    level order traversal

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTree {
@@ -17,6 +19,7 @@ public class BinaryTree {
         System.out.println("\nPostOrder Traverse Recursive way");
         binaryTree.postOrderRecursively(binaryTree.root);
         binaryTree.postOrderIteratively(binaryTree.root);
+        binaryTree.levelOrderTraverse(binaryTree.root);
     }
 
     public void createTree() {
@@ -125,6 +128,23 @@ public class BinaryTree {
                 } else {
                     current = temp;
                 }
+            }
+        }
+    }
+
+    public void levelOrderTraverse(TreeNode root) {
+        System.out.println("\nLevel Order Traversal");
+        if (root == null) return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            System.out.print(temp.data + " ");
+            if (temp.left != null) {
+                queue.offer(temp.left);
+            }
+            if (temp.right != null) {
+                queue.offer(temp.right);
             }
         }
     }
