073f4da4c88614c98a1e1851598056bd55da0d20
GautamViki <vikacse@gmail.com>
2022-10-16 12:15:11 +0530

    bubble sort

--- src/SortingAndSearching/BubbleSort.java
+++ src/SortingAndSearching/BubbleSort.java
@@ -12,16 +12,16 @@ public class BubbleSort {
 
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            boolean flag = false;
+            boolean isSwapped = false;
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    flag = true;
+                    isSwapped = true;
                 }
             }
-            if (!flag) {
+            if (!isSwapped) {
                 break;
             }
         }
