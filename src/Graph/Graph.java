28cd27d3c9c519bce63d3b70a7cb5eb27a638eaf
GautamViki <vikacse@gmail.com>
2022-10-17 04:34:49 +0530

    create graph

---
+++ src/Graph/Graph.java
@@ -0,0 +1,32 @@
+package Graph;
+
+import java.util.Arrays;
+
+public class Graph {
+    private int[][] adjMatrix;
+    private int edge;
+
+    private int vtx;
+
+    public Graph(int node) {
+        this.adjMatrix = new int[node][node];
+        this.vtx = node;
+        this.edge = 0;
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(4);
+        graph.addNode(0, 1);
+        graph.addNode(1, 2);
+        graph.addNode(2, 3);
+        graph.addNode(3, 0);
+        System.out.println(graph);
+    }
+
+
+    public void addNode(int u, int v) {
+        this.adjMatrix[u][v] = 1;
+        this.adjMatrix[v][u] = 1;
+        edge++;
+    }
+}
