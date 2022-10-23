9f5a8bc9e890ced2d06f9c102266837a7f5e1c20
GautamViki <vikacse@gmail.com>
2022-10-23 08:27:40 +0530

    comparator

--- src/ComparatorUses/RollComparison.java
+++ src/ComparatorUses/RollComparison.java
@@ -1,4 +1,11 @@
 package ComparatorUses;
 
-public class RollComparison {
+import java.util.Comparator;
+
+public class RollComparison implements Comparator<Student> {
+    public int compare(Student s1, Student s2) {
+        if (s1.getRoll() < s2.getRoll()) return 1;
+        else if (s1.getRoll() > s2.getRoll()) return -1;
+        else return 0;
+    }
 }
